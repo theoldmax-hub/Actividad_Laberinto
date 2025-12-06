849be98bff1a011b6b331c807313128ccb154552
theoldmax-hub <makschernykh001346@gmail.com>
2025-12-07 00:57:19 +0100

    Commit 2
    Fixed sin function
    Created Laberint

--- Assets/Scripts/ItemMeshMovement.cs
+++ Assets/Scripts/ItemMeshMovement.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class MapMeshMovement : MonoBehaviour
 {
-    public float constantRotation = 0.5f;
-    private float y = 0;
+    public float rotaionSpeed = 30F;
+    private float initialRotationY = 0;
     public float runningTime;
-    public float amplitude = 0.008f;
+    public float amplitude = 0.3f;
     public float sin;
+    private Vector3 initialPosition;
     private Vector3 newPosition;
     
     void Start()
     {
-        
+      initialPosition = transform.position;  
     }
 
     // Update is called once per frame
@@ -20,8 +21,8 @@ public class MapMeshMovement : MonoBehaviour
         runningTime += Time.deltaTime;
         sin = amplitude * Mathf.Sin(runningTime);
         newPosition = new Vector3(0f, sin, 0f);
-        transform.position += newPosition;
+        transform.position = initialPosition + newPosition;
 
-        transform.Rotate(0f, y + constantRotation, 0f);
+        transform.Rotate(0f, initialRotationY + rotaionSpeed * Time.deltaTime, 0f);
     }
 }
