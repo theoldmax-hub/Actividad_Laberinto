c875d9df001f78c4be644a9ee0e69ca0cfe10ea5
Overtank079 <34961961+Overtank079@users.noreply.github.com>
2026-02-08 16:15:24 +0100

    Ataque enemigo

--- Assets/Scripts/Combat/DaggerCollision.cs
+++ Assets/Scripts/Combat/DaggerCollision.cs
@@ -7,34 +7,18 @@ public class DaggerCollision : MonoBehaviour
     [SerializeField] float KnockbackForce = 10f;
     [SerializeField] Animator animatora;
     Animator animator;
+    public bool canDamage = true;
     private void Start()
     {
         animator = animatora.GetComponent<Animator>();
     }
-    private void OnTriggerEnter(Collider other)
+ 
+    private void OnTriggerStay(Collider other)
     {
-        /*    if (other.transform.parent.CompareTag("Enemy") && animator.GetBool("Attack"))
-            {
-
-
-                int EnemyHealth = other.GetComponent<CombatSystemScript>().HealthPoints;
-                EnemyHealth -= PlayerInformation.AttackPower;
-                other.GetComponent<CombatSystemScript>().HealthPoints = EnemyHealth;
-
-                Debug.Log("Ouch!");
-                Rigidbody knockback = other.GetComponent<Rigidbody>();
-                if (TryGetDamageable(other, out IDamageable damageable))
-                damageable.TakeDamage((float)PlayerInformation.AttackPower,Player);
-                if(knockback != null)
-                {
-                    knockback.AddForce(Player.transform.forward * KnockbackForce, ForceMode.Impulse);
-                }
-
-        }
-        */
-        if (TryGetDamageable(other, out IDamageable damageable) && animator.GetBool("Attack"))
+        if (TryGetDamageable(other, out IDamageable damageable) && animator.GetBool("Attack") && canDamage)
         {
             Debug.Log("Ouch!");
+            canDamage = false;
             Rigidbody knockback = other.GetComponent<Rigidbody>();
             damageable.TakeDamage((float)PlayerInformation.AttackPower, Player);
             if (knockback != null)
@@ -42,9 +26,16 @@ public class DaggerCollision : MonoBehaviour
                 knockback.AddForce(Player.transform.forward * KnockbackForce, ForceMode.Impulse);
             }
         }
+
     }
 
-
+    private void Update()
+    {
+        if (!animator.GetBool("Attack"))
+        {
+            canDamage = true;
+        }
+    }
     private bool TryGetDamageable(Collider col, out IDamageable damageable)
     {
         foreach (var mb in col.GetComponentsInParent<MonoBehaviour>())
